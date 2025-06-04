af4d82a02fe847bf1f858322d8bbd17902691f66
Jian-Zhi <114330064+Jian-Zhi@users.noreply.github.com>
2025-06-04 17:03:12 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,9 +6,8 @@ from model.resNet import ResNet18
 backbone = ResNet18(pretrained=False)
 
 if __name__ == '__main__':
-    #train_task = OceanTrainer(backbone=backbone)
-    # train_task.run()
-
+    train_task = OceanTrainer()
+    
     # train_task = ProtoNetTrainer()
     train_task.TRAIN_EPOCH_N = 300 #300
     train_task.TEST_N = 2000 #2000
